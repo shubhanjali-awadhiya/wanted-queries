f5438eb07c3f7454e1d5cac39434391f24c6554b
lex.baldiwn <starwarbler@gmail.com>
2018-02-12 13:44:42 -0700

    created tests for the enter wanted portion

---
+++ nightwatch/test_data/data.js
@@ -0,0 +1,127 @@
+module.exports = {
+    test1: {
+        inputs: {
+            hdr: '.aaa !@8',
+            mke: 'B',
+            oai: 'AAAA5555',
+            nam: 'A#',
+            hgt: '601',
+            wgt: '',
+            hai: 'Bl',
+            off: 'DR3#',
+            oln: ' ',
+            ols: 'A',
+            lic: '1223CR',
+            lis: 'A',
+        },
+        dateInputs: {
+            dow: '12311899',
+            old: '12311899',
+            lid: '12311899'
+        },
+        dropDownPicks: {
+            sex: 'female',
+            rac: 'asian',
+        }
+    },
+    test2: {
+        inputs: {
+            hdr: 'aaaa BBBB !@#$ ..199',
+            mke: 'BB$$?',
+            oai: 'AAAA555556',
+            nam: '!@#$ .... (">?") 300',
+            hgt: '6101',
+            wgt: '1455',
+            hai: 'BlckBlckBBB',
+            off: 'drunken $#$?.!55',
+            oln: 'HHHHH 1234 !@#$',
+            ols: 'ZA',
+            lic: '1223CRIME',
+            lis: 'ZA',
+        },
+        dateInputs: {
+            dow: '02312019',
+            old: '02312022',
+            lid: '02312022'
+        },
+        dropDownPicks: {
+            sex: 'unknown',
+            rac: 'unknown',
+        }
+    },
+    test3: {
+        inputs: {
+            hdr: 'aaaa !@#$',
+            mke: 'B$',
+            oai: 'AAAA55555',
+            nam: 'A3#',
+            hgt: '001',
+            wgt: '1',
+            hai: 'Blk',
+            off: 'DR3$!',
+            oln: '#',
+            ols: 'AK',
+            lic: '123CR',
+            lis: 'AK',
+        },
+        dateInputs: {
+            dow: '01011900',
+            old: '01011900',
+            lid: '01011900'
+        },
+        dropDownPicks: {
+            sex: 'female',
+            rac: 'asian',
+        }
+    },
+    test4: {
+        inputs: {
+            hdr: 'aaaa BBBB !@#$ 1119',
+            mke: 'BB$$',
+            oai: 'AAAA55555',
+            nam: 'First LAST !@#$ 1111 (">?") 30',
+            hgt: '999',
+            wgt: '999',
+            hai: 'BlckBlckBB',
+            off: 'drunken $#?$1!5',
+            oln: 'HHHHH 1234 !@#$ "11"',
+            ols: 'WI',
+            lic: '123CRIME',
+            lis: 'WI',
+        },
+        dateInputs: {
+            dow: '02092018',
+            old: '01292018',
+            lid: '01292018'
+        },
+        dropDownPicks: {
+            sex: 'unknown',
+            rac: 'unknown',
+        }
+    },
+    test5: {
+        inputs: {
+            hdr: '123456789',
+            mke: 'BBB',
+            oai: 'AAAA55555',
+            nam: 'Lavar Ball',
+            hgt: '606',
+            wgt: '240',
+            hai: 'Black',
+            off: 'Fraud',
+            oln: '123',
+            ols: 'AK',
+            lic: '',
+            lis: '',
+        },
+        dateInputs: {
+            dow: '02092018',
+            old: '02092018',
+            lid: ''
+        },
+        dropDownPicks: {
+            sex: 'male',
+            rac: 'asian',
+        }
+    }
+}
---
+++ nightwatch/test_data/selectors.js
@@ -0,0 +1,56 @@
+module.exports = {
+
+    fields: {
+        hdr: 'input[name="hdrInput"]',
+        mke: 'input[name="mkeInput"]',
+        oai: 'input[name="oriInput"]',
+        nam: 'input[name="namInput"]',
+        hgt: 'input[name="hgtInput"]',
+        wgt: 'input[name="wgtInput"]',
+        hai: 'input[name="haiInput"]',
+        off: 'input[name="offInput"]',
+        oln: 'input[name="olnInput"]', //Driver's license #
+        ols: 'input[name="olsInput"]', // DL state
+        lic: 'input[name="licInput"]', //license plate
+        lis: 'input[name="lisInput"]', //license state
+        //
+        //wid: 'input[name="widInput"]',
+        //resInput: 'input[name="resInput"]', //reason for cancellation
+        //doc: 'input[name="datInput"]',  //date of cancellation
+    },
+    dateFields: {
+        dow: 'input[name="dowInput"]', //date of warrant
+        old: 'input[name="oldInput"]', // DL expiration date
+        lid: 'input[name="lidInput"]',
+    },
+    dropDowns: {
+        sex: {
+            dropDown: 'select[name="sexInput"]',
+            female: 'option[value="F"]',
+            male: 'option[value="M"]',
+            other: 'option[value="O"]',
+            unknown: 'option[value="U"]'
+        },
+        rac: {
+            dropDown: 'select[name="racInput"]',
+            asian: 'option[value="A"]',
+            black: 'option[value="B"]',
+            hispanic: 'option[value="H"]',
+            americanIndian: 'option[value="I"]',
+            white: 'option[value="W"]',
+            unknown: 'option[value="U"]'
+        }
+    },
+    buttons: {
+        menu: 'div[class="bm-burger-button"]',
+        submit: 'button[id="saveBtn"]',
+        clear: 'button[id="clearBtn"]',
+        home: 'p[name="homeOption"]',
+        enterWanted: 'p[name="enterOption"]',
+        modifyWanted: 'p[name="modifyOption"]',
+        cancelWanted: 'p[name="cancelOption"]',
+        X: 'div[class="bm-cross-button"]'
+    },
+    resultList: 'td[class="results"]',
+    page: 'div[id="pageWrap"]'
+ }
